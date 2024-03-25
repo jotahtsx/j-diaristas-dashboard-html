04b2771e41e19e3c3845bd2eabb5ddff85b67ab5
jotahtsx <joaomanoelskai@gmail.com>
2024-03-25 07:16:11 -0300

    feat: fish dropdown

---
+++ js/main.js
@@ -0,0 +1,39 @@
+const dropdown = () => {
+    const _variables = {
+        main: "j-dropdown",
+        menu: "j-down__menu",
+        target: "data-dropdown-target",
+        active: "j-active",
+    }
+
+    const dropDown = [...document.querySelectorAll(`.${_variables.main}`)]
+
+    document.addEventListener("click", e => {
+        const target = e.target.closest(`.${_variables.main}`)
+
+        const targetedeMenu = e.target.closest(`.${_variables.menu}`)
+
+        if (!target || targetedeMenu) return
+
+        const targetId = target.querySelector(`[${_variables.target}]`).getAttribute(_variables.target)
+
+        const activeMenu = document.querySelector(`#${targetId}`)
+
+        const nonTargeted = dropDown.map(drop => {
+            const nonActiveId = drop.querySelector(`[${_variables.target}]`).getAttribute(_variables.target)
+            const nonActive = document.querySelector(`#${nonActiveId}`)
+
+            return nonActive;
+
+        })
+
+        const filterExcepActive = nonTargeted.filter(target =>
+            target !== activeMenu)
+
+        filterExcepActive.forEach(drop => drop.classList.remove(_variables.active))    
+
+        if (activeMenu) activeMenu.classList.toggle(_variables.active)
+    })
+}
+
+dropdown()
